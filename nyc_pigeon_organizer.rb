1372a913d355995cff1327dd771d29a953e76a42
Shau Yin Luk <ivan.luk@icloud.com>
2020-02-27 23:47:49 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+  data.each do |c_g_l, value|
+    value.each do |type, names|
+      names.each do |ind|
+        if pigeon_list[ind] == nil 
+          pigeon_list[ind] = {}
+        end
+        if pigeon_list[ind][c_g_l] == nil
+          pigeon_list[ind][c_g_l] = []
+        end
+        pigeon_list[ind][c_g_l].push("#{type}") 
+      end
+    end
+  end
+  pigeon_list
 end
