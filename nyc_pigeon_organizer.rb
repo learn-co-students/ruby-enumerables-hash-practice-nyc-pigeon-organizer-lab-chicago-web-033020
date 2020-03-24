725f5cd5f88bdfe719cfcbdcc896fe3dc3779aa6
Derek Williams <jogamer130@gmail.com>
2020-03-24 03:35:29 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,43 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+  pigeon_list = {}
+
+
+  male_pigeons = data[:gender][:male]
+  male_pigeon_list = {}
+
+  female_pigeons = data[:gender][:female]
+  female_pigeon_list = {}
+
+  male_pigeons.each do |name|
+    male_pigeon_list[name] = {:color => [], :gender => ["male"], :lives => []}
+  end
+
+  female_pigeons.each do |name|
+   female_pigeon_list[name] = {:color => [], :gender => ["female"], :lives => []}
+  end
+
+  pigeon_list = male_pigeon_list.merge(female_pigeon_list)
+
+  
+  color_of_pigeon = data[:color]
+
+  pigeon_list.each do |name, attribute_hash|
+    color_of_pigeon.each do |color_sym, name_array|
+      if name_array.include?(name)
+        (pigeon_list[name][:color]).push(color_sym.to_s)
+      end
+    end
+  end
+
+  
+  residence_of_pigeon = data[:lives]
+
+  pigeon_list.each do |name, attribute_hash|
+    residence_of_pigeon.each do |location, name_array|
+      if name_array.include?(name)
+        (pigeon_list[name][:lives]).push(location)
+      end
+    end
+  end
+  pigeon_list
+end 
