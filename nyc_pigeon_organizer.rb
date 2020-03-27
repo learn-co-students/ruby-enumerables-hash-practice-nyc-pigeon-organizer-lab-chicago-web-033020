a3e9ac42ad24f9157c5194e5554ec687c1d71bba
Joseph E Auz <joseph.auz77@gmail.com>
2020-03-27 06:01:31 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+ final_pigeon = {}
+ data.each do |about, value|
+   value.each do |stats, all_names|
+     all_names.each do |name|
+       if final_pigeon[name] == nil 
+         final_pigeon[name] = {}
+       end 
+       if final_pigeon[name][about] == nil
+         final_pigeon[name][about] = []
+       end 
+       final_pigeon[name][about].push(stats.to_s)
+     end 
+   end 
+ end 
+ final_pigeon
 end
