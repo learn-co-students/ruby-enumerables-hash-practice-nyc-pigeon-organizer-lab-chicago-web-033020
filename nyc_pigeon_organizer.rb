f2ad3ddad41c7d2658aa4aeca2aa9a84b971c435
Haley Rodzos <haley.rodzos@gmail.com>
2020-03-28 23:36:02 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,27 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+final_hash = {
+  
+}
+  
+  data.each do |thing, pigeons|
+  pigeons.each do |detail_ab_thing, more_pigeons|
+  more_pigeons.each do |name|
+      
+      if !final_hash.has_key?(name)
+      final_hash[name] = {}
+    end
+        
+      if !final_hash[name].has_key?(thing)
+       final_hash[name][thing] = []
+      end
+      
+    if !final_hash[name][thing].include?(detail_ab_thing)
+    final_hash[name][thing] << detail_ab_thing.to_s
+    end
+  end
+end
+end
+  final_hash
 end
